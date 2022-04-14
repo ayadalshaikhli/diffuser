0ce2d9d37485f990c605606c9cd7ae369b6df3fd
ayadalshaikhli <79942630+ayadalshaikhli@users.noreply.github.com>
2022-04-14 02:32:03 -0700

    trying to fix vercel

--- next.config.js
+++ next.config.js
@@ -1,23 +1,4 @@
 module.exports = {
-  webpack(config) {
-    // Perform customizations to webpack config
-    config.module.rules.push({
-      // shader import support
-      test: /\.glsl$/,
-      use: [
-        {
-          loader: "emit-file-loader",
-          options: {
-            name: "dist/[path][name].[ext]",
-          },
-        },
-        "babel-loader",
-        "webpack-glsl-loader",
-      ],
-    });
-
-    return config;
-  },
   env: {
     SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
     SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN,
