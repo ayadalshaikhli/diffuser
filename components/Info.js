4f402d42a7215d7a94c3fb8fd0e2a73b44c64da3
ayadalshaikhli <79942630+ayadalshaikhli@users.noreply.github.com>
2022-04-14 03:19:30 -0700

    maybe

--- components/Info.js
+++ components/Info.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-
+function Info() {
   return (
     <div>
       <div className="text-white h-1/2 w-full flex flex-col justify-around md:flex-row md:items-start md:space-y-0 md:space-x-4 lg:space-x-8 max-w-6xl  mx-auto p-7 border-b-2 border-t-2 mt-16">
@@ -17,7 +17,7 @@ import Image from "next/image";
           <h1 className="text-center font-extrabold text-xl mb-3">
             How do I use it?
           </h1>
-          <p className="text-lg dd">
+          <p className="text-lg ">
             Simply fill the E-Flame with water, add your favorite essential
             oils, plug it in and turn on! It's as easy as that.
           </p>
