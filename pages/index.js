ed6e631c480a1f09507f87894f6e9b33af1062f6
ayadalshaikhli <79942630+ayadalshaikhli@users.noreply.github.com>
2022-04-14 02:49:10 -0700

    trying to fix vercel

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,6 @@ export default function Home({ products }) {
       <div className="wrap">
         <Hero products={products} />
         <FrontPage />
-
         <ProductList products={products} />
       </div>
     </div>
